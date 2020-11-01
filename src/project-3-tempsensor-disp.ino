1b42bfe317938d1ef2a9eae77b8045522c8ea5db
Frank <frank@edynamics.co>
2020-11-01 08:17:54 -0500

    TMP102 library

--- src/project-3-tempsensor-disp.ino
+++ src/project-3-tempsensor-disp.ino
@@ -14,9 +14,6 @@ IOTboard iot_kit;
 // setup() runs once, when the device is first turned on.
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Lab2: OLED sensor (task 2a)");
-
     iot_kit.start();
 
     iot_kit.printToDisplay("Hello");
